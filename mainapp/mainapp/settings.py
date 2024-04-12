b2425f95eca31697febcc6e694181cd9709ff069
Harsh Malik <harsh@convin.ai>
2024-04-12 23:46:54 +0530

    upadted requiremenets

--- mainapp/mainapp/settings.py
+++ mainapp/mainapp/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-277+&-=pd_19fy_$zg6zv5%@7lw)ratuyz3%!8u3+^h3767-f5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
